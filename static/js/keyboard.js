ac2bd5bb1418fa045a986bc4b55f980f791bf92e
Adriana Castaneda <adriana.lcs316@gmail.com>
2018-01-17 08:37:25 -0600

    add staff note code

--- static/js/keyboard.js
+++ static/js/keyboard.js
@@ -1,5 +1,6 @@
 var whiteKeys = ["C","D","E","F","G","A","B"]
-var posOne = ["","","","","","",""]
+var positionWhiteKeys = ["top:9rem, left:8rem;","top:7.5rem, left:10rem;","top:5rem, left:8rem;"]
+var positionBlackKeys = ["top:6.5rem, left:8rem;"]
 
 $("#keyboard div p").on("click", function(){
 	var lol = $(this).html();
